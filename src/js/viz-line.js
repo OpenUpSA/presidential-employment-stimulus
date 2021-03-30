d3e37dae9b5c4b9dc4d79d403857a88b8719ee72
“rikusv” <rikusv@gmail.com>
2021-03-31 00:47:39 +0200

    Rough timeline chart (still not great)

--- src/js/viz-line.js
+++ src/js/viz-line.js
@@ -1,9 +1,21 @@
 import $ from 'jquery';
-import { d3 } from './d3';
 import { formatCount } from './utils';
 
-const SELECTOR = '.feature-value__line-graph';
-const $template = $(SELECTOR).clone().empty();
+const HEIGHT = 70;
+const MARGIN = 20;
+
+const CONTAINER_SELECTOR = '.feature-value__line-graph';
+const PERIOD_SELECTOR = '.line-graph__period';
+const DATE_SELECTOR = '.line-graph__period_label.line-graph__period_label--1';
+const MARKER_SELECTOR = '.line-graph__period_marker';
+const TOOLTOP_SELECTOR = '.line-graph__period_tooltip';
+
+const $containerTemplate = $(CONTAINER_SELECTOR).clone();
+const $periodTemplate = $containerTemplate.find(PERIOD_SELECTOR).first().clone(true, true);
+$containerTemplate.empty();
+
+const marginPercent = (MARGIN / HEIGHT) * 100;
+const maxPercent = 100 - marginPercent;
 
 export class VizLine {
   constructor($parent, values) {
@@ -13,89 +25,38 @@ export class VizLine {
   }
 
   render() {
-    const parseDate = d3.timeParse('%Y%m');
-    const data = this._values.map((d) => ({
-      ...d,
-      date: parseDate(d.month),
+    const max = this._values.reduce((res, record) => Math.max(res, record.value), 0);
+    const min = this._values.reduce((res, record) => Math.min(res, record.value), 0);
+    const range = max - min;
+    const data = this._values.map((record) => ({
+      ...record,
+      y: ((max - record.value) / range) * maxPercent + marginPercent,
+      height: ((record.value - min) / range) * (HEIGHT - MARGIN),
     }));
-    const id = `line-chart-${Math.round(Math.random() * 1000000)}`;
-    this._$parent.append($template.clone().attr('id', id));
-    const selector = `#${id}`;
-    const margin = {
-      top: 30,
-      right: 50,
-      bottom: 20,
-      left: 30,
-    };
-    const width = 300;
-    const height = 80;
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-    const xScale = d3.scaleTime().range([0, innerWidth]);
-    const yScale = d3.scaleLinear().range([innerHeight, 0]);
-    xScale.domain(d3.extent(data, (d) => d.date));
-    yScale.domain([0, d3.max(data, (d) => d.value)]);
-    const xAxis = d3.axisBottom().scale(xScale).ticks(data.length);
-    const valueline = d3.line()
-      .x((d) => xScale(d.date))
-      .y((d) => yScale(d.value));
-    const svg = d3.select(selector)
-      .append('svg')
-      .attr('xmlns:xhtml', 'http://www.w3.org/1999/xhtml')
-      .attr('viewBox', `0 0 ${width} ${height}`)
-      .append('g')
-      .attr('transform', `translate(${margin.left},${margin.top})`);
-    // const tooltip = svg.append('foreignObject')
-    //   .attr('width', '10')
-    //   .attr('height', '10')
-    //   .style('overflow', 'visible')
-    //   .style('opacity', 0)
-    //   .attr('alignment-baseline', 'middle');
-
-    // tooltip
-    //   .append('xhtml:div')
-    //   .attr('style', 'padding: 4px 6px; border-radius: 4px; background-color: black; box-shadow: 0 3px 3px 0 rgb(0 0 0 / 10%); color: white; line-height: 1; white-space: nowrap;')
-    //
-    //   .text('hello');
-    const tooltip = svg.append('g')
-      .append('text')
-      .style('opacity', 0)
-      .attr('font-size', '10px')
-      .attr('alignment-baseline', 'middle');
-    svg.append('path')
-      .attr('class', 'line')
-      .attr('stroke', 'lightgrey')
-      .attr('fill', 'none')
-      .attr('stroke-width', 1)
-      .attr('d', valueline(data));
-    svg.append('g')
-      .attr('transform', `translate(0,${innerHeight})`)
-      .call(xAxis);
-    const bisect = d3.bisector((d) => d.date).right;
-    const mousemove = (evt) => {
-      const x0 = xScale.invert(d3.pointer(evt)[0]);
-      const i = bisect(data, x0, 1) - 1;
-      const selectedData = data[i];
-      tooltip
-        .html(formatCount(selectedData.value))
-        .attr('x', xScale(selectedData.date))
-        .attr('y', yScale(selectedData.value) - 15);
-    };
-    svg.selectAll('markers')
-      .data(data)
-      .enter()
-      .append('circle')
-      .attr('r', 3)
-      .attr('cx', (d) => xScale(d.date))
-      .attr('cy', (d) => yScale(d.value))
-      .attr('fill', '#124069')
-      .on('mouseover', () => {
-        // aaa.attr('display', 'block')
-        tooltip.style('opacity', 1);
-      })
-      .on('mousemove', mousemove)
-      .on('mouseout', () => {
-        tooltip.style('opacity', 0);
-      });
+    const $graph = $containerTemplate.clone();
+    data.forEach((record, i) => {
+      const $period = $periodTemplate.clone(true, true);
+      $period.find(DATE_SELECTOR).text(record.name);
+      const $tooltip = $period.find(TOOLTOP_SELECTOR)
+        .text(formatCount(record.value));
+      $period
+        .on('mouseover', () => $tooltip.show())
+        .on('mouseout', () => $tooltip.hide());
+      $period.find(MARKER_SELECTOR).css('margin-bottom', `${record.height}px`);
+      if (i !== 0) {
+        const x1 = 2;
+        const x2 = 98;
+        const y1 = data[i - 1].y;
+        const y2 = record.y;
+        const svg = `<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100"><line x1="${x1}" x2="${x2}" y1="${y1}" y2="${y2}" style="stroke:rgba(0,0,0,0.5);stroke-width:1" /></svg>`;
+        const uri = encodeURI(`data:image/svg+xml;base64,${btoa(unescape(encodeURIComponent(svg)))}`);
+        $period.css({
+          'background-image': `url(${uri})`,
+          'background-size': '100% 100%',
+        });
+      }
+      $graph.append($period);
+    });
+    this._$parent.append($graph);
   }
 }
