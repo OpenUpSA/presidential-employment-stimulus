cfe6cc138fa4746182b01e2545298f67974725c0
“rikusv” <rikusv@gmail.com>
2021-04-13 00:07:02 +0200

    Over-Target achievement suggestion

--- src/js/viz-progress.js
+++ src/js/viz-progress.js
@@ -4,6 +4,7 @@ import { formatPercentage } from './utils';
 const COLORS = {
   done: 'rgb(5,65,104)',
   notdone: 'rgb(220,220,220)',
+  exceeded: 'rgb(37, 120, 75)',
 };
 
 export class VizProgress {
@@ -17,11 +18,53 @@ export class VizProgress {
     const size = 56;
     const margin = 7;
     const radius = size / 2;
-    const data = { done: this._quotient, notdone: 1 - this._quotient };
     const pie = d3.pie()
       .value((d) => d.value);
     pie.sort((a, b) => a.key < b.key);
-    const pieData = pie(d3.entries(data));
+    const data = {};
+    if (this._quotient > 1) {
+      data.done = 1;
+      data.exceeded = this._quotient - 1;
+      const endA = this._quotient * 2 * Math.PI;
+      pie.endAngle(endA);
+    } else {
+      data.done = this._quotient;
+      data.notdone = 1 - this._quotient;
+    }
+    const circ = 2 * Math.PI;
+    const pieData = [
+      {
+        data: {
+          key: 'done',
+          value: this._quotient,
+        },
+        index: 0,
+        startAngle: 0,
+        endAngle: this._quotient > 1 ? circ : circ * this._quotient,
+        padAngle: 0,
+        value: this._quotient,
+      },
+    ];
+    let key;
+    let value;
+    if (this._quotient > 1) {
+      key = 'exceeded';
+      value = this._quotient - 1;
+    } else {
+      key = 'notdone';
+      value = 1 - this._quotient;
+    }
+    pieData.push({
+      data: {
+        key,
+        value,
+      },
+      index: 1,
+      startAngle: pieData[0].endAngle,
+      endAngle: pieData[0].endAngle + circ * value,
+      padAngle: 0,
+      value,
+    });
     const svg = d3.select(this._parentSelector)
       .append('svg')
       .attr('width', size)
@@ -40,8 +83,8 @@ export class VizProgress {
       .enter()
       .append('path')
       .attr('d', d3.arc()
-        .innerRadius(radius - margin)
-        .outerRadius(radius))
+        .innerRadius((d) => radius - (margin * (d.data.key === 'exceeded' ? 1.5 : 1)))
+        .outerRadius((d) => radius - (d.data.key === 'exceeded' ? margin + 1 : 0)))
       .attr('fill', (d) => COLORS[d.data.key])
       .style('stroke-width', 0);
   }
