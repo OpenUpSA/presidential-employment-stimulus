8a71007d5f46f6d5a3304a98635925c8bed87c5c
“rikusv” <rikusv@gmail.com>
2021-04-12 23:01:34 +0200

    Import webflow, fix selectors, add bar targets

--- src/js/header.js
+++ src/js/header.js
@@ -1,6 +1,7 @@
 import $ from 'jquery';
 
 const SELECTOR = '.tab-pane .tab-header';
+const TITLE_SELECTOR = '.tab-title';
 
 const $template = $(SELECTOR).first().clone(true, true);
 
@@ -16,7 +17,7 @@ export class Header {
   render() {
     const $el = $template.clone(true, true);
     $el.find('.header__label').text(''); // TODO Put this back if needed (e.g. "Dec '20 - Jan '21")
-    $el.find('h2').text(this._title);
+    $el.find(TITLE_SELECTOR).text(this._title);
     $el.find('h3').text(this._lead);
     $el.find('p').text(this._paragraph);
     this._$parent.append($el);
--- src/js/viz-bars.js
+++ src/js/viz-bars.js
@@ -1,24 +1,34 @@
 import $ from 'jquery';
+import { FORMATTERS } from './utils';
 
-const CONTAINER_SELECTOR = '.feature-value__bar-chart';
-const ROW_SELECTOR = '.bar-chart__row.small.w-inline-block';
+const CONTAINER_SELECTOR = '.components .feature-value__bar-chart';
+const ROW_SELECTOR_NO_TARGET = '.components .bar-chart__row:not(.bar-chart__row--with-target)';
+const ROW_SELECTOR_WITH_TARGET = '.components .bar-chart__row.bar-chart__row--with-target';
+const ROW_INNER_SELECTOR = '.bar-chart__row_inner';
 
 const BAR_SELECTOR = '.bar-chart__row_bar';
+const BAR_TARGET_SELECTOR = '.bar-chart__row_target';
 const BAR_TOOLTIP_SELECTOR = '.bar-chart__row_tooltip';
+const BAR_TARGET_TOOLTIP_SELECTOR = '.bar-chart__row_target-tooltip';
 const BAR_CAT_LABEL_SELECTOR = '.bar-chart__row_label';
 const BAR_VAL_LABEL_SELECTOR = '.bar-chart__row_value';
 
 const $containerTemplate = $(CONTAINER_SELECTOR).first().clone(true, true).empty();
-const $rowTemplate = $(ROW_SELECTOR).first().clone(true, true);
+const $rowTemplateNoTarget = $(ROW_SELECTOR_NO_TARGET).first().clone(true, true);
+const $rowTemplateWithTarget = $(ROW_SELECTOR_WITH_TARGET).first().clone(true, true);
 
 export class VizBars {
   constructor($parent, rows, lookup) {
     this._$parent = $parent;
     this._rows = rows;
     this._lookup = lookup;
-    this._max = this._rows
+    const maxValue = this._rows
       .map((row) => row.value)
       .reduce((max, curr) => Math.max(max, curr), 0);
+    const maxTarget = this._rows
+      .map((row) => (row.value_target ? row.value_target : 0))
+      .reduce((max, curr) => Math.max(max, curr), 0);
+    this._max = Math.max(maxValue, maxTarget);
     this.render();
   }
 
@@ -26,13 +36,21 @@ export class VizBars {
     const $el = $containerTemplate.clone(true, true);
     this._rows.forEach((row) => {
       const width = Math.round((row.value / this._max) * 100);
-      const $row = $rowTemplate.clone(true, true);
+      const target = Math.round((row.value_target / this._max) * 100);
+      const $row = (row.value_target ? $rowTemplateWithTarget : $rowTemplateNoTarget)
+        .clone(true, true);
       $row.find(BAR_SELECTOR).width(`${width}%`);
+      $row.find(BAR_TARGET_SELECTOR).css('left', `${target}%`);
+      const $targetTooltip = $row.find(BAR_TARGET_TOOLTIP_SELECTOR).text(`TARGET: ${FORMATTERS.count(row.value_target)}`);
+      $row.find(ROW_INNER_SELECTOR)
+        .on('mouseover', () => $targetTooltip.show())
+        .on('mouseout', () => $targetTooltip.hide());
+      const $label = $row.find(BAR_CAT_LABEL_SELECTOR).text(row.key.toUpperCase());
       const $tooltip = $row.find(BAR_TOOLTIP_SELECTOR).text(this._lookup[row.key]);
-      $row.on('mouseover', () => $tooltip.show());
-      $row.on('mouseout', () => $tooltip.hide());
-      $row.find(BAR_CAT_LABEL_SELECTOR).text(row.key.toUpperCase());
-      $row.find(BAR_VAL_LABEL_SELECTOR).text(row.value);
+      $label
+        .on('mouseover', () => $tooltip.show())
+        .on('mouseout', () => $tooltip.hide());
+      $row.find(BAR_VAL_LABEL_SELECTOR).text(FORMATTERS.count(row.value));
       $el.append($row);
     });
     this._$parent.append($el);
